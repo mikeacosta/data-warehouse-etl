8abec2f72d1b7716e04965494ec473f454edd3be
mikeacosta <mike@acosta.net>
2019-12-16 23:26:22 -0800

    initial commit

---
+++ etl.py
@@ -0,0 +1,37 @@
+import configparser
+import psycopg2
+from sql_queries import copy_table_queries, insert_table_queries
+
+
+def load_staging_tables(cur, conn):
+    print('loading staging tables')
+    for query in copy_table_queries:
+        print('running query: ' + query)
+        cur.execute(query)
+        conn.commit()
+
+
+def insert_tables(cur, conn):
+    print('inserting data')
+    for query in insert_table_queries:
+        print('running query: ' + query)
+        cur.execute(query)
+        conn.commit()
+
+
+def main():
+    config = configparser.ConfigParser()
+    config.read('dwh.cfg')
+
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    cur = conn.cursor()
+    print('connected to Redshift cluster')
+    
+    load_staging_tables(cur, conn)
+    insert_tables(cur, conn)
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
